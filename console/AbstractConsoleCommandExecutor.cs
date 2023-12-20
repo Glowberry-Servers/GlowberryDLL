362c76e4251221b70c1d6af6d5528f9c369cabde
MrKelpy <alexandresilva.coding@gmail.com>
2023-12-20 22:35:12 +0000

    Moved the console parsing and executor features into the DLL.

---
+++ console/AbstractConsoleCommandExecutor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using glowberry.api.server;
+using glowberry.common.handlers;
+
+namespace glowberry.console
+{
+    /// <summary>
+    /// This class is responsible for taking commands through its methods and executing API calls
+    /// to the backend.
+    ///
+    /// The methods created in here must use the provided API to interact with the backend, and their signature
+    /// must be «public void Command_(Command Name) (ConsoleCommand command)».
+    ///
+    /// To register a command's description and usage, follow the format in the App.config file.
+    /// </summary>
+    public class AbstractConsoleCommandExecutor
+    {
+        
+        /// <summary>
+        /// The API used to interact with the backend.
+        /// </summary>
+        protected ServerAPI API { get; } = new ServerAPI();
+        
+        /// <summary>
+        /// The output handler used to write messages to the console.
+        /// </summary>
+        protected MessageProcessingOutputHandler OutputHandler { get; } = new MessageProcessingOutputHandler(Console.Out);
+        
+        /// <summary>
+        /// Using reflection, accesses all the methods within this class and tries to run the one matching
+        /// the command name.
+        /// If not found, write the help message.
+        /// </summary>
+        public void ExecuteCommand(ConsoleCommand command)
+        {
+            try
+            {
+                MethodInfo method = this.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .FirstOrDefault(m => m.Name.ToLower() == "command_" + command.Command.Replace("-", "_").ToLower());
+                
+                // If the method exists, run it and return.
+                if (method != null)
+                {
+                    method.Invoke(this, new object[] {command});
+                    return;
+                }
+                
+                // If the method does not exist, write the help message.
+                OutputHandler.Write( $@"Command '{command.Command}' not found. Use 'help' for a list of possible commands.");
+            }
+            
+            // If the method throws an exception, try to expose the inner exception.
+            catch (TargetInvocationException e)
+            {
+                if (e.InnerException != null) throw e.InnerException;
+                throw;
+            }
+        }
+    }
+}
---
+++ console/ConsoleCommandParser.cs
@@ -0,0 +1,26 @@
+﻿namespace glowberry.console
+{
+    /// <summary>
+    /// This class is responsible for parsing out the commands send from the console
+    /// and forwarding them into the AbstractConsoleCommandExecutor class, to be executed.
+    /// </summary>
+    public static class ConsoleCommandParser
+    {
+        /// <summary>
+        /// Parses the command line arguments into a ConsoleCommand object.
+        /// </summary>
+        /// <param name="commandLineArguments">The command line arguments to parse</param>
+        public static ConsoleCommand Parse(string[] commandLineArguments)
+        {
+            // Creates the command and arguments for the ConsoleCommand object.
+            string command = commandLineArguments[0];
+            string[] arguments = new string[commandLineArguments.Length - 1];
+            
+            // Adds the arguments to the array (skips the first one, which is the command).
+            for (int i = 1; i < commandLineArguments.Length; i++)
+                arguments[i - 1] = commandLineArguments[i];
+
+            return new ConsoleCommand(command, arguments);
+        }
+    }
+}
