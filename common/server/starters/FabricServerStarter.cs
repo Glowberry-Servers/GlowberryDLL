c5b8a756174b0637d949b5517424ffdcdd444f0f
MrKelpy <alexandresilva.coding@gmail.com>
2024-04-04 17:22:39 +0100

    Added the fabric server starter, and thus it's fully integrated.

---
+++ common/server/starters/FabricServerStarter.cs
@@ -0,0 +1,19 @@
+﻿using glowberry.common.handlers;
+
+namespace glowberry.common.server.starters
+{
+    /// <summary>
+    /// This class handles everything related to starting fabric servers.
+    /// </summary>
+    internal class FabricServerStarter : AbstractServerStarter
+    {
+        /// <summary>
+        /// Main constructor for the FabricServerStarter class. Defines the start-up arguments for the server, as well
+        /// as the "other arguments" that are passed to the server.
+        /// </summary>
+        /// <param name="outputHandler">The output system to use while logging the messages.</param>
+        public FabricServerStarter(MessageProcessingOutputHandler outputHandler) : base(" ", "-jar %RAM_ARGUMENTS% \"%SERVER_JAR%\"", outputHandler)
+        {
+        }
+    }
+}
