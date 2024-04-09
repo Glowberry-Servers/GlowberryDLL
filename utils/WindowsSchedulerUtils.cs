84b0a12f2616bb82b32ad2408a85bcbcc1975d66
MrKelpy <alexandresilva.coding@gmail.com>
2024-04-09 13:54:27 +0100

    Finished the windows scheduler utils, essentially doing what the registry should've done. Testing time!

--- utils/WindowsSchedulerUtils.cs
+++ utils/WindowsSchedulerUtils.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using LaminariaCore_General.common;
 using LaminariaCore_General.utils;
+using Microsoft.Win32;
 using Microsoft.Win32.TaskScheduler;
 
 namespace glowberry.utils;
@@ -17,12 +18,12 @@ public class WindowsSchedulerUtils
     /// <summary>
     /// The name of the section that contains the scripts for the server.
     /// </summary>
-    private static string ScriptsSectionName = "gb-scripts";
+    private static string ScriptsSectionName { get; set; } = "gb-scripts";
         
     /// <summary>
     /// The name of the boot script that will be added to the Windows Registry.
     /// </summary>
-    private static string BootScriptName = "boot.bat";
+    private static string BootScriptName { get; set; } = "boot.bat";
 
     /// <summary>
     /// Registers a server to the Windows Task Scheduler to start it on boot.
@@ -36,6 +37,9 @@ public class WindowsSchedulerUtils
         string taskName = $"Glowberry-{serverSection.SimpleName}-bootstart";
         string bootScriptFilepath = Path.Combine(serverSection.AddSection(ScriptsSectionName).SectionFullPath, BootScriptName);
 
+        // Check if the server is already in the scheduler. If it is, then we don't need to do anything.
+        if (IsServerInScheduler(serverSection)) return true;
+
         if (serverSection.GetFirstDocumentNamed("boot.bat") == null)
         {
             // Create the .bat file to be added to the Windows Registry
@@ -45,6 +49,9 @@ public class WindowsSchedulerUtils
 
         try
         {
+            // Check if the task already exists. If it does, then we don't need to do anything.
+            if (taskService.FindTask(taskName) != null) return true;
+            
             // Creates the task definition and registers it into the Windows Task Scheduler
             TaskDefinition definition = taskService.NewTask();
             definition.RegistrationInfo.Description = $"Glowberry server {serverSection.SimpleName} startup task";
@@ -58,6 +65,48 @@ public class WindowsSchedulerUtils
 
         return true;
     }
+    
+    /// <summary>
+    /// Removes both the server from the Windows Task Scheduler and the boot script from the server's section.
+    /// </summary>
+    /// <param name="serverSection">The section of the server to delete the schedule of</param>
+    /// <returns>Whether the deletion was a success or not</returns>
+    public static bool RemoveServerFromTaskScheduler(Section serverSection)
+    {
+        using TaskService taskService = new TaskService();
+        string taskName = $"Glowberry-{serverSection.SimpleName}-bootstart";
+        string bootScriptFilepath = Path.Combine(serverSection.AddSection(ScriptsSectionName).SectionFullPath, BootScriptName);
+
+        try
+        {
+            // If the server isn't in the scheduler, then we don't need to do anything.
+            if (taskService.FindTask(taskName) != null)
+                taskService.RootFolder.DeleteTask(taskName);
+
+            // If the boot script exists, then we can delete it.
+            if (File.Exists(bootScriptFilepath)) File.Delete(bootScriptFilepath);
+        }
+        catch { return false; }
+
+        return true;
+    }
+    
+    /// <summary>
+    /// Checks for two things: If the server is registered under the windows scheduler and if the boot script
+    /// exists. If any of those two aren't A.O.K, then the server isn't properly configured for boot running.
+    /// </summary>
+    /// <param name="serverSection">The section of the server to be checked</param>
+    /// <returns>Whether the server is registered and the boot script exists</returns>
+    public static bool IsServerInScheduler(Section serverSection)
+    {        
+        using TaskService taskService = new TaskService();
+        string taskName = $"Glowberry-{serverSection.SimpleName}-bootstart";
+        string bootScriptFilepath = Path.Combine(serverSection.AddSection(ScriptsSectionName).SectionFullPath, BootScriptName);
+
+        return File.Exists(bootScriptFilepath) && taskService.FindTask(taskName) != null;
+    }
+    
+    
     
     /// <summary>
     /// Builds the startup command for the server to be run on system boot.
